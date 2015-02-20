0308f7fa5d6f7c6f2758ca7044ac7e2f9a2bb9e3
Daniel Schröder <daniel.schroeder@gravitymedia.de>
2015-02-21 00:22:13 +0100

    Default theme

--- sami.config.php
+++ sami.config.php
@@ -14,7 +14,7 @@ $iterator = Finder::create()
     ->in(__DIR__ . '/src');
 
 return new Sami($iterator, array(
-    'theme' => 'enhanced',
+    'theme' => 'default',
     'title' => 'Metadata API',
     'build_dir' => __DIR__ . '/docs',
     'cache_dir' => __DIR__ . '/cache',
