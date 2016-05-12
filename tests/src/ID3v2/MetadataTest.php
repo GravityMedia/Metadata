3fda6fff81d70025877139e0e04b2ade495db75e
Daniel Schröder <daniel.schroeder@evolver.de>
2016-05-12 18:04:03 +0200

    Added tests for frames

--- tests/src/ID3v2/MetadataTest.php
+++ tests/src/ID3v2/MetadataTest.php
@@ -8,6 +8,7 @@
 namespace GravityMedia\MetadataTest\ID3v2;
 
 use GravityMedia\Metadata\ID3v2\Metadata;
+use GravityMedia\Metadata\ID3v2\Restriction;
 use GravityMedia\Metadata\ID3v2\Version;
 use GravityMedia\MetadataTest\Helper\ResourceHelper;
 
@@ -31,6 +32,40 @@ class MetadataTest extends \PHPUnit_Framework_TestCase
         $tag = $metadata->read();
         $this->assertNotNull($tag);
         $this->assertSame(Version::VERSION_23, $tag->getVersion());
+        $this->assertSame(0, $tag->getRevision());
+        $this->assertSame(0, $tag->getPadding());
+        $this->assertSame(0, $tag->getCrc32());
+
+        $frames = $tag->getFrames();
+        $this->assertCount(7, $frames);
+
+        $frame = $frames[0];
+        $this->assertEquals('COMM', $frame->getName());
+        $this->assertEquals('ID3v1.x Comment', $frame->getContent());
+
+        $frame = $frames[1];
+        $this->assertEquals('TORY', $frame->getName());
+        $this->assertEquals('2003', $frame->getContent());
+
+        $frame = $frames[2];
+        $this->assertEquals('TIT2', $frame->getName());
+        $this->assertEquals('Title', $frame->getContent());
+
+        $frame = $frames[3];
+        $this->assertEquals('TPE1', $frame->getName());
+        $this->assertEquals('Artist', $frame->getContent());
+
+        $frame = $frames[4];
+        $this->assertEquals('TALB', $frame->getName());
+        $this->assertEquals('Album', $frame->getContent());
+
+        $frame = $frames[5];
+        $this->assertEquals('TRCK', $frame->getName());
+        $this->assertEquals('12', $frame->getContent());
+
+        $frame = $frames[6];
+        $this->assertEquals('TCON', $frame->getName());
+        $this->assertEquals('(7)Hip-Hop', $frame->getContent());
 
         //var_dump($tag->getFrames());
     }
@@ -46,6 +81,44 @@ class MetadataTest extends \PHPUnit_Framework_TestCase
         $tag = $metadata->read();
         $this->assertNotNull($tag);
         $this->assertSame(Version::VERSION_24, $tag->getVersion());
+        $this->assertSame(0, $tag->getRevision());
+        $this->assertSame(0, $tag->getCrc32());
+        $this->assertSame(-1, $tag->getRestriction(Restriction::RESTRICTION_TAG_SIZE));
+        $this->assertSame(-1, $tag->getRestriction(Restriction::RESTRICTION_TEXT_ENCODING));
+        $this->assertSame(-1, $tag->getRestriction(Restriction::RESTRICTION_TEXT_FIELDS_SIZE));
+        $this->assertSame(-1, $tag->getRestriction(Restriction::RESTRICTION_IMAGE_ENCODING));
+        $this->assertSame(-1, $tag->getRestriction(Restriction::RESTRICTION_IMAGE_SIZE));
+
+        $frames = $tag->getFrames();
+        $this->assertCount(7, $frames);
+
+        $frame = $frames[0];
+        $this->assertEquals('COMM', $frame->getName());
+        $this->assertEquals('ID3v1.x Comment', $frame->getContent());
+
+        $frame = $frames[1];
+        $this->assertEquals('TIT2', $frame->getName());
+        $this->assertEquals('Title', $frame->getContent());
+
+        $frame = $frames[2];
+        $this->assertEquals('TPE1', $frame->getName());
+        $this->assertEquals('Artist', $frame->getContent());
+
+        $frame = $frames[3];
+        $this->assertEquals('TALB', $frame->getName());
+        $this->assertEquals('Album', $frame->getContent());
+
+        $frame = $frames[4];
+        $this->assertEquals('TRCK', $frame->getName());
+        $this->assertEquals('12', $frame->getContent());
+
+        $frame = $frames[5];
+        $this->assertEquals('TCON', $frame->getName());
+        $this->assertEquals('(7)Hip-Hop', $frame->getContent());
+
+        $frame = $frames[6];
+        $this->assertEquals('TDOR', $frame->getName());
+        $this->assertEquals('2003', $frame->getContent());
 
         //var_dump($tag->getFrames());
     }
