e37014bc7e87e4fb64ce5048c44ed26fbd3a355e
Daniel Schröder <daniel.schroeder@evolver.de>
2016-05-12 18:08:20 +0200

    Initial commit

---
+++ src/ID3v2/Restriction.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * This file is part of the ID3 project.
+ *
+ * @author Daniel Schröder <daniel.schroeder@gravitymedia.de>
+ */
+
+namespace GravityMedia\Metadata\ID3v2;
+
+/**
+ * ID3v2 restriction class.
+ *
+ * @package GravityMedia\Metadata\ID3v2
+ */
+class Restriction
+{
+    /**
+     * Tag size restriction.
+     */
+    const RESTRICTION_TAG_SIZE = 0;
+
+    /**
+     * Text encoding restriction.
+     */
+    const RESTRICTION_TEXT_ENCODING = 1;
+
+    /**
+     * Text fields size restriction.
+     */
+    const RESTRICTION_TEXT_FIELDS_SIZE = 2;
+
+    /**
+     * Image encoding restriction.
+     */
+    const RESTRICTION_IMAGE_ENCODING = 3;
+
+    /**
+     * Image size restriction.
+     */
+    const RESTRICTION_IMAGE_SIZE = 4;
+
+    /**
+     * Valid values.
+     *
+     * @var int[]
+     */
+    protected static $values = [
+        self::RESTRICTION_TAG_SIZE,
+        self::RESTRICTION_TEXT_ENCODING,
+        self::RESTRICTION_TEXT_FIELDS_SIZE,
+        self::RESTRICTION_IMAGE_ENCODING,
+        self::RESTRICTION_IMAGE_SIZE
+    ];
+
+    /**
+     * Return valid values.
+     *
+     * @return int[]
+     */
+    public static function values()
+    {
+        return static::$values;
+    }
+}
