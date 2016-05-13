aa0bd047f3a626110395c676912273ca19f09be9
Daniel Schröder <daniel.schroeder@gravitymedia.de>
2016-05-13 23:28:37 +0200

    Initial commit

---
+++ src/ID3v2/Writer/FrameWriter.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of the Metadata package.
+ *
+ * @author Daniel Schröder <daniel.schroeder@gravitymedia.de>
+ */
+
+namespace GravityMedia\Metadata\ID3v2\Writer;
+
+use GravityMedia\Metadata\ID3v2\StreamContainer;
+
+/**
+ * ID3v2 frame writer class.
+ *
+ * @package GravityMedia\Metadata\ID3v2\Writer
+ */
+class FrameWriter extends StreamContainer
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Set ID3v2 frame name.
+     *
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Write ID3v2 frame data data.
+     *
+     * @return $this
+     */
+    public function write()
+    {
+        $this->getStream()->seek($this->getOffset());
+
+        $this->getStream()->write($this->name);
+
+        return $this;
+    }
+}
